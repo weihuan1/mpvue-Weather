c99ac7740dfabfd08b3117f65f30f116e022aa78
fengweihuan <1004743308@qq.com>
2018-07-03 17:09:31 +0800

    初始化项目

---
+++ src/pages/counter/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="counter-warp">
+    <span>{{ msg }}</span>
+    <p>Vuex counter：{{ count }}</p>
+    <p>
+      <button @click="increment">+</button>
+      <button @click="decrement">-</button>
+    </p>
+
+    <a href="/pages/index/main" class="home">去往首页</a>
+  </div>
+</template>
+
+<script>
+// Use Vuex
+import store from './store'
+import fetch from '@/utils/fetch'
+export default {
+  computed: {
+    count () {
+      return store.state.count
+    }
+  },
+  data () {
+    return {
+      msg: 'hehe'
+    }
+  },
+  async created () {
+    console.log(fetch)
+    const res = await fetch('http://www.weather.com.cn/data/sk/101010100.html')
+    console.log(res)
+  },
+  methods: {
+    increment () {
+      store.commit('increment')
+    },
+    decrement () {
+      store.commit('decrement')
+    }
+  }
+}
+</script>
+
+<style>
+.counter-warp {
+  text-align: center;
+  margin-top: 100px;
+}
+.home {
+  display: inline-block;
+  margin: 100px auto;
+  padding: 5px 10px;
+  color: blue;
+  border: 1px solid blue;
+}
+</style>
---
+++ src/utils/fetch.js
@@ -0,0 +1,28 @@
+
+export default function fetch (url, options = {}) {
+  const { methods = 'GET', body = {}, showError = true } = options
+  return new Promise((resolve, reject) => {
+    wx.request({
+      url,
+      data: body,
+      methods,
+      header: {
+        'content-type': 'application/json'
+      },
+      success (res) {
+        if (showError) {
+          if (res.statusCode < 200 || res.statusCode > 300) {
+            reject(res.data || {})
+          }
+          // if (Number(res.data.status) !== 0) {
+          //   reject(res.data || {})
+          // }
+        }
+        resolve(res.data || {})
+      },
+      complete (res) {
+        // TODO
+      }
+    })
+  })
+}
